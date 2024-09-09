dbf0338ac6f6545f70a7e998a0bc938f55d878b1
Haitham Amireh <70890685+HaithamAmireh@users.noreply.github.com>
2024-09-09 15:34:18 +0300

    Add files via upload

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  -- add gruvbox
+  { "maxmx03/fluoromachine.nvim" },
+  { "Rigellute/shades-of-purple.vim" },
+  { "pineapplegiant/spaceduck" },
+  { "hachy/eva01.vim" },
+  { "rebelot/kanagawa.nvim" },
+  { "rafi/awesome-vim-colorschemes" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "materialBox",
+    },
+  },
+}
